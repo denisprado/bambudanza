eb28f86088b45c44391549be01632558de812e3b
denisforigo <denisforigo@gmail.com>
2020-08-07 11:40:32 -0300

    Adding Profesoras  relation widget

--- src/templates/tarifa-post.js
+++ src/templates/tarifa-post.js
@@ -20,7 +20,6 @@ export const TarifaPostTemplate = ({
     <Escuela>
       {helmet || ''}
       <Flex>
-
         <Box as='main'>
           <Heading>{title}</Heading>
           <Text>{price}</Text>
