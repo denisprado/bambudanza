1a430fa1b22a08e70b6359b3aa42587392ec1920
denisforigo <denisforigo@gmail.com>
2020-08-08 18:17:47 -0300

    clasificación de los programas

--- src/components/ProgramasRoll.js
+++ src/components/ProgramasRoll.js
@@ -28,27 +28,15 @@ const ProgramasRoll = ({ data }) => {
   return (
     <Flex mt={4} sx={{ minHeight: '50vh' }}>
       <Box mt={2} sx={{ minWidth: '20vh' }}>
-        <Box mt={4}>
+        <Box >
           <Heading as={'h3'}>Tipos</Heading>
-          {filterTipo &&
-            <Box m={2}>
-              <Text>Filtro: </Text>
-              <Badge bg={'muted'} color={'primary'}>{filterTipo}</Badge>
-              <Link onClick={() => setFilterTipo(null)}><FiXCircle /></Link>
-            </Box>
-          }
+
           {(tipos.map(t => <Box><Link onClick={() => setFilterTipo(t)}>{t}</Link></Box>))}
         </Box>
 
-        <Box mt={4}>
+        <Box mt={3}>
           <Heading as={'h3'}>Nivel</Heading>
-          {filterNivel &&
-            <Box m={2}>
-              <Text>Filtro: </Text>
-              <Badge bg={'muted'} color={'primary'}>{filterNivel}</Badge>
-              <Link onClick={() => setFilterNivel(null)}><FiXCircle /></Link>
-            </Box>
-          }
+
           {(niveis.map(nivel => <Box><Link onClick={() => setFilterNivel(nivel)}>{nivel}</Link></Box>))}
         </Box>
       </Box>
@@ -57,39 +45,59 @@ const ProgramasRoll = ({ data }) => {
         programas.map(({ node: programa }) => (
           (filterTipo === null || filterTipo === programa.frontmatter.tipo) &&
           (filterNivel === null || filterNivel === programa.frontmatter.nivel) &&
-          <Card as='article'
-            sx={{
-              minWidth: '584px'
-            }}
-            key={programa.id}>
-            <Link
-              to={programa.fields.slug}
-            >
-              <AspectRatio ratio={3 / 2}>
-                <PreviewCompatibleImage
-                  imageInfo={{
-                    image: programa.frontmatter.featuredimage,
-                    alt: `featured image thumbnail for programas ${programa.frontmatter.title}`,
-                  }}
-                />
-              </AspectRatio>
-            </Link>
-            <Flex>
 
-              <Heading
+          <Box>
+            <Box>
+
+              {filterNivel &&
+                <>
+                  <Badge bg={'muted'} color={'primary'}>{filterNivel}</Badge>
+                  <Link onClick={() => setFilterNivel(null)}><FiXCircle /></Link>
+                </>
+              }
+              {filterTipo &&
+                <>
+                  <Badge bg={'muted'} color={'primary'}>{filterTipo}</Badge>
+                  <Link onClick={() => setFilterTipo(null)}><FiXCircle /></Link>
+                </>
+              }
+            </Box>
+
+
+            <Card as='article'
+              sx={{
+                minWidth: '584px'
+              }}
+              key={programa.id}>
+              <Link
                 to={programa.fields.slug}
               >
-                <Text>
-                  {programa.frontmatter.title}
-
-                </Text>
-              </Heading>
-              <Link sx={{ marginLeft: 'auto' }}>
-                <Badge>{programa.frontmatter.tipo}</Badge>
+                <AspectRatio ratio={16 / 9}>
+                  <PreviewCompatibleImage
+                    imageInfo={{
+                      image: programa.frontmatter.featuredimage,
+                      alt: `featured image thumbnail for programas ${programa.frontmatter.title}`,
+                    }}
+                  />
+                </AspectRatio>
               </Link>
-            </Flex>
+              <Flex>
 
-          </Card>
+                <Heading
+                  to={programa.fields.slug}
+                >
+                  <Text>
+                    {programa.frontmatter.title}
+
+                  </Text>
+                </Heading>
+                <Link sx={{ marginLeft: 'auto' }}>
+                  <Badge>{programa.frontmatter.tipo}</Badge>
+                </Link>
+              </Flex>
+
+            </Card>
+          </Box>
         ))
       }
     </Flex >
