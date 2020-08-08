b4340dacb6b705cc087a84c1dfed7d4021584aba
denisforigo <denisforigo@gmail.com>
2020-08-08 15:29:38 -0300

    clasificación de los programas

--- src/pages/escuela/index.js
+++ src/pages/escuela/index.js
@@ -1,23 +1,26 @@
 import React from 'react'
-import { Container, Flex, Image, jsx } from 'theme-ui'
+import { Box, Flex, Image, jsx } from 'theme-ui'
 import Link from '../../components/Link'
 import Layout from '../../components/Layout'
 import escuelaHeader from '../../img/escuela-header.jpg'
 
 const Escuela = ({ children }) => (
-    <Layout bg={'muted'}>
+    <Layout >
         <Image src={escuelaHeader} width={'100%'}></Image>
 
-        <Flex as='nav' mb={2} sx={{
+        <Flex as='nav' pl={4} mb={2} sx={{
             'a': {
                 padding: '3',
-            }
+            },
+            borderBottom: '1px solid muted'
         }}>
             <Link p={4} to='/escuela/programas'>Programas</Link>
             <Link p={4} to='/escuela/profesoras'>Profesoras</Link>
             <Link p={4} to='/escuela/tarifas'>Tarifas</Link >
         </Flex >
-        {children}
+        <Box p={4}>
+            {children}
+        </Box>
 
     </Layout >
 )
