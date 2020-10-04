68e8e7cd9f308a073c6f69b05b976bf3b045faa4
denisforigo <denisforigo@gmail.com>
2020-10-04 17:25:28 -0300

    index page with image

--- src/pages/index.md
+++ src/pages/index.md
@@ -1,21 +1,22 @@
 ---
 templateKey: index-page
 title: Bambudanza
-image: /img/default-image.png
+image: /img/grid-poesia.jpg
 heading: Bienvenido!
 subheading: Arte en movimiento
-description: >-
-    Mi oído escucha
-    Mi corazón palpita
-    El alma despierta
-    Mi cuerpo cobra vida 
-    Se mueve
-    Mi cadera y mis pies desobedecen,
-    Cada uno tiene su propia independencia,
-    Mis manos acarician el aire …disfruto
-    Brazos ,hombros, cuello, piernas, pies, caderas, manos…
-    Ahora todo tiene sentido
-    Soy un cuerpo en movimiento
-    Soy danza
-    Soy poesía…
+description: Arte en movimiento
 ---
+
+Mi oído escucha
+Mi corazón palpita
+El alma despierta
+Mi cuerpo cobra vida
+Se mueve
+Mi cadera y mis pies desobedecen,
+Cada uno tiene su propia independencia,
+Mis manos acarician el aire …disfruto
+Brazos ,hombros, cuello, piernas, pies, caderas, manos…
+Ahora todo tiene sentido
+Soy un cuerpo en movimiento
+Soy danza
+Soy poesía…
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -2,9 +2,10 @@ import { graphql } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 /** @jsx jsx */
-import { Box, Flex, Image, jsx } from 'theme-ui'
+import { Container, Box, Grid, Image, jsx } from 'theme-ui'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 export const IndexPageTemplate = ({
   image,
@@ -17,10 +18,23 @@ export const IndexPageTemplate = ({
   const PageContent = contentComponent || Content
 
   return (
-    <>
-      {description}
-      <PageContent className="content" content={content} />
-    </>
+    <Container>
+      <Grid gap={2}
+        columns={['3fr 5fr']}>
+        <Box>
+          <PageContent className="content" content={content} />
+        </Box>
+        <Box >
+          <PreviewCompatibleImage
+            imageInfo={{
+              image: image,
+              alt: `featured image thumbnail ${title}`,
+              styles: { width: '100%' }
+            }}
+          />
+        </Box>
+      </Grid>
+    </Container >
   )
 }
 
@@ -61,23 +75,22 @@ IndexPage.propTypes = {
 export default IndexPage
 
 export const pageQuery = graphql`
-  query IndexPageTemplate
-        {
-          markdownRemark(frontmatter: {templateKey: {eq: "index-page" } }) {
-          html
-          frontmatter {
-            title
-            heading
-            subheading
-            description
-            image {
-              childImageSharp {
-              fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
+query IndexPageTemplate {
+  markdownRemark(frontmatter: {templateKey: {eq: "index-page"}}) {
+    html
+    frontmatter {
+      title
+      heading
+      subheading
+      description
+      image {
+        childImageSharp {
+          fluid(maxWidth: 640, quality: 100) {
+            ...GatsbyImageSharpFluid
           }
         }
       }
     }
   }
+}   
 `
